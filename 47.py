2d0a3bdc7c08e4ff6705dc29dc646def13e52ca2
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2023-04-26 02:31:26 +0600

    First commit

---
+++ 47.py
@@ -0,0 +1,4 @@
+import file_1
+
+print(file_1.sum(57898,66))
+print(file_1.mystring("Swadhin"))
---
+++ file_1.py
@@ -0,0 +1,17 @@
+def sum(a,b):
+    return a+b
+
+
+def mystring(string):
+    return f"I love you {string}"
+
+#if __name__ == "main": 
+#if we remove "#" then 47.py will not print these arguments
+    
+print(f"The sum is :",sum(77,3))
+print(mystring("Rafsan"))
+    
+
+
+
+
