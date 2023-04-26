32290d4d6897b531b41a6c88a29c44f4c8aefa58
RafsanSwadhin <126243472+RafsanSwadhin@users.noreply.github.com>
2023-04-26 18:17:08 +0600

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,14 @@
+import numbers
+
+
+grocery = ['Harpic','Vimbar','potato','alo','bindo',56]
+print(grocery[5])
+num = [6,7,5,3,15,14]
+num.sort()
+print(num)
+num.reverse()
+print(num)
+print(max(num))
+print(min(num))
+num.insert(1,99)
+print(num)
